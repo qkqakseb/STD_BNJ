fab0212bbd9885ad1dd4f1dc8952ec7943ac90c6
leeSun <encyless@gmail.com>
2022-05-15 14:02:08 +0900

    20220515

---
+++ CSharpStudy/STD_BNJ2/MapleStory.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+
+namespace STD_BNJ2
+{
+    public class MapleStory
+    {
+        public void Run()
+        {
+            // 메이플 실행
+            // 직업선택            
+            // 뭘 할것인지 ?
+            // 던전에 간다
+            // 상점에 간다
+
+            // 던전에 가면 ?
+            // - 전투를 한다
+
+            // 상점에 가면 ?
+            // - 무기, 포션을 살 수 있다.
+
+
+            // 직업을 나열
+            // 전사, 법사, 아처
+
+            // 오늘 절차지향적 게임 개발
+            // 다음 객체지향적 게임 개발
+
+            // 유저는 체력, 공격, 속도를 갖고, 직업별로 조금씩 다르다!
+            int hp = 10;
+            int userAtk = 0;
+            int speed = 0;
+            string job = "모험가";
+
+            // 턴제 게임
+            // 스피드가 빠르면 먼저 공격한다.
+            // 공격을 받으면 체력이 공격력 만큼 감소 한다.
+
+            Console.WriteLine("메이플 세계에 오신 것을 환영합니다!");
+
+            Console.WriteLine("직업을 선택해 주세요!");
+            Console.WriteLine("1. 전사 \t 2. 법사 \t 3. 아처");
+            var select = Console.ReadLine();
+
+            if(select == "1")
+            {
+                job = "전사";
+                Console.WriteLine("전사를 선택하셨습니다!");
+                hp = 500;
+                userAtk = 100;
+                speed = 5;
+            }
+            else if(select == "2")
+            {
+                job = "법사";
+                Console.WriteLine("법사를 선택하셨습니다!");
+                hp = 300;
+                userAtk = 120;
+                speed = 3;
+            }
+            else if (select == "3")
+            {
+                job = "아처";
+                Console.WriteLine("아처를 선택하셨습니다!");
+                hp = 400;
+                userAtk = 80;
+                speed = 10;
+            }
+
+            // 유저 능력치
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("--- User Status ---");
+
+            Console.WriteLine("직업 : " + job);
+            Console.WriteLine("HP : " + hp);
+            Console.WriteLine("Atk : " + userAtk);
+            Console.WriteLine("Speed : " + speed);
+
+
+            // 유저의 선택을 제공
+            Console.WriteLine();
+            Console.WriteLine("무엇을 할것인가요?");
+
+            Console.WriteLine("1. 상점 \t 2. 던전");
+            select = Console.ReadLine();
+
+            if (select == "1")
+            {
+                Console.WriteLine("상점에 진입합니다!");
+            }
+            else if(select == "2")
+            {
+                Console.WriteLine("던전에 진입합니다!");
+            }
+        }
+    }
+}
---
+++ CSharpStudy/STD_BNJ2/MatchingSystemProgram.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace STD_BNJ2_MatchingSystem
+{
+    public class Solution
+    {
+
+    }
+
+    // 서든
+    // 래드 팀 또는 블루 팀의 평균 레벨
+    // 매칭시스템 개선
+
+    public class MatchingSystem
+    {
+        public class MatchingResult
+        {
+            public List<User> redTeam = new List<User>();
+            public List<User> blueTeam = new List<User>();
+        }
+
+        // 데이터, 기능을 갖고있음.
+        // 매칭을 하기 위해 필요한 데이터,
+        public MatchingResult Match(User[] inUsers)
+        {
+            MatchingResult result = new MatchingResult();
+            int eachTeamCount = (int)(inUsers.Length * 0.5f + 1); // 10 => 6
+
+            List<User> userList = new List<User>();
+            userList.AddRange(inUsers);
+
+            userList.Sort(CompareScore);
+
+            // 로직 여기서 부터
+            // 프로그래머가 하는 일
+
+            // 기획자가 하는일 -> 방식으로
+            // 기획자는 룰을 정해줌
+            // Score 제일높은 사람은, 제일 낮은 사람이랑 팀 되게 해주세요.
+
+            // 배열 
+            // 20 30 40 10 50 25
+
+            // 제일 높은 사람 = 50, 40, 30
+            // 제일 낮은 사람 = 10, 20, 25
+
+            // 정렬
+            // Sort
+            //      10, 20, 25, 30, 40, 50
+            //배열    0,  1,  2,  3,  4,  5
+
+            // (50, 10)  , (40, 20),  (30, 25)
+            //  (5, 0)      (4, 1)     (3,2)
+
+            // 이렇게 팀을 시켜 줘야 겠다.
+            // 이걸 코드로!
+
+            // -----
+
+            // 1. 정렬한다.
+            // 2. 인덱스 별로 매칭을 한다.
+
+            // 10명이라서 
+            int index1 = userList.Count - 1;
+            int index2 = 0;
+            bool isBlueTeam = false; // 번갈아 가면서 들어올 수 있도록
+            for (int i = 0; i < eachTeamCount; ++i)
+            {
+                if (index1 <= index2)
+                    break;
+
+                var bestScroeUser = userList[index1];
+                var lowScoreUser = userList[index2];
+                if (isBlueTeam) // true
+                {
+                    result.blueTeam.Add(bestScroeUser);
+                    result.blueTeam.Add(lowScoreUser);
+                    isBlueTeam = false; // false
+                }
+                else // false
+                {
+                    result.redTeam.Add(bestScroeUser);
+                    result.redTeam.Add(lowScoreUser);
+                    isBlueTeam = true; // true
+                }
+                index1--; // 9
+                index2++; // 0
+            }
+            return result;
+        }
+
+        // -1 0 1
+        private int CompareScore(User x, User y)
+        {
+            if (x.Score > y.Score)
+                return -1;
+            else if (x.Score == y.Score)
+                return 0;
+
+            return 1;
+        }
+    }
+
+    public class User
+    {
+        public enum Team
+        {
+            Red,
+            Blue,
+        }
+        public int Score;
+        public Team team;
+    }
+
+    public class Human
+    {
+        // 데이터,
+        int age;
+        float height;
+        int iq;
+        int sex;
+        public string name;
+
+        // 기능
+        public void Eat()
+        {
+            Console.WriteLine("먹는다!");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("잔다!");
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine("My Name Is " + name);
+            Console.WriteLine("how are you!?");
+        }
+    }
+
+
+    internal class MatchingSystemProgram
+    {
+        public void Run()
+        {
+            User[] users = new User[12];
+            for (int i = 0; i < users.Length; ++i)
+            {
+                users[i] = new User();
+                users[i].Score = (i * 5);
+                users[i].team = User.Team.Red;
+
+                if (i >= 5)
+                    users[i].team = User.Team.Blue;
+            }
+
+
+            MatchingSystem matchingSystem = new MatchingSystem();
+            var result = matchingSystem.Match(users);
+
+            foreach (var user in result.blueTeam)
+            {
+                Console.WriteLine("blue team : " + user.Score);
+            }
+
+            foreach (var user in result.redTeam)
+            {
+                Console.WriteLine("red team : " + user.Score);
+            }
+        }
+    }
+}
