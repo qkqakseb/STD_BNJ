7f1a68c20e2a95c7f800dd6a355fe3afded054cf
leeSun <encyless@gmail.com>
2022-05-15 13:24:02 +0900

    initialize repo

---
+++ CSharpStudy/STD_BNJ2/ExplainHeapAndStack.cs
@@ -0,0 +1,47 @@
+﻿namespace STD_BNJ2
+{
+    public class ExplainStackClass
+    {
+        public int a;
+        public int b;
+        public int c;
+    }
+
+    public struct ExplainStackStruct
+    {
+
+        public int a;
+        public int b;
+        public int c;
+    }
+
+    public class ExplainHeapAndStack
+    {
+
+        public void OnFunction(int index)
+        {
+            // 함수 호출
+            // index : 스택에 저장
+
+            // 지역변수들 : 스택에 저장
+            int sA;
+            int sB;
+            int sC;
+
+            // 생성, 가르키는자
+            ExplainStackClass classA;
+
+            // 생성, 값 - 구조체는 값을 의미,
+            ExplainStackStruct structB = new ExplainStackStruct();
+
+            // classA라는 변수는 스택
+            // new 힙에 저장됨?
+            // classA는 그럼 힙이야? 스택이야?
+        }
+
+        public void OtherFunction()
+        {
+
+        }
+    }
+}
---
+++ CSharpStudy/STD_BNJ2/ValueReferenceExplain.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace STD_BNJ2
+{
+    public class ValueReferenceExplain
+    {
+        // strcut는 값 형식
+
+        public struct Dog
+        {
+            public int a;
+            public int b;
+        }
+
+        public class TestDog
+        {
+
+        }
+
+        public void Explain()
+        {
+            // 값형식과 참조형식의 설명
+            // '복사' 가 일어나냐 안 일어 나냐
+            // class 는 참조형식
+            // 대입 연산으로는 메모리가 할당이 잃어나지 않는다.
+
+
+            // class 생성 new 메모리 할당
+            // 같은 참조를 하고 있다.
+            TestDog tdog = new TestDog();
+            TestDog chiwawa2 = tdog;
+
+            Dog dog = new Dog(); // 메모리에 할당한다.
+            dog.a = 5;
+            dog.b = 10;
+
+            dog.a = 8;
+
+            // struct
+            // 복사
+            // dog의 "값" 들을 chiwawa에 "넣는다", "값" 만 복사를 한다.
+            Dog chiwawa = dog; // 치와와에 도그를 넣음 , dog.a == 5, dog.b == 10
+
+            // struct , class
+
+            dog.a = 20;
+
+            Console.WriteLine("chiwawa " + chiwawa.a); // chiwawa의 a는 5
+            // 컴파일 타임은 코드를 짜고 난 뒤에 실행 전에 
+            // 런타임은 - 코드가 완전히 실행 될 때
+        }
+    }
+}
